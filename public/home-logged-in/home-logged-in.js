c41d51569af2803ac65607b8ceadbe4550f659db
Anthony Vidovic <avidovic@uoguelph.ca>
2021-07-15 14:23:10 -0400

    added account info frontend

--- public/home-logged-in/home-logged-in.js
+++ public/home-logged-in/home-logged-in.js
@@ -1,6 +1,13 @@
 const userProfileIcon = document.getElementById("user-profile-icon");
 const modelContainer = document.getElementById("model_container");
+const updateAccountInput = document.getElementById("update-account-input");
+const userProfileBackBtn = document.getElementById("profile-settings-back-btn");
 
 userProfileIcon.addEventListener("click", function () {
 	modelContainer.classList.add("show");
+	updateAccountInput.value = "";
+});
+
+userProfileBackBtn.addEventListener("click", function () {
+	modelContainer.classList.remove("show");
 });

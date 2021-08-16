a9a4d06f257ffa59095570787fcecfb59d79d386
Anthony Vidovic <avidovic@uoguelph.ca>
2021-08-16 12:58:57 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const flash = require("connect-flash");
-const session = require("express-session");
+const session = require("cookie-session");
 const dotenv = require("dotenv");
 const passport = require("passport");
 

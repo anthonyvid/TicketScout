e8aa9bc974ba721522b006a2cd8c74d710ad4298
Anthony Vidovic <avidovic@uoguelph.ca>
2021-09-30 10:14:09 -0400

    redirect errors to my email

--- config/errors.js
+++ config/errors.js
@@ -1,8 +1,7 @@
-import { db } from "../db.js";
-const errorsCollection = db.collection("errors");
+import { sendEmail } from "../models/Helper.js";
 
 /**
- * Function will catch all errors that bubble up to it 
+ * Function will catch all errors that bubble up to it
  * @param {function} fn function from controller passed in
  * @returns none
  */
@@ -10,20 +9,43 @@ export const catchError = (fn) => (req, res, next) => {
 	Promise.resolve(fn(req, res, next)).catch(async (error) => {
 		console.error(error);
 
-		// Add error into database for referencing later
-		await errorsCollection.insertOne({
-			date: new Date().toLocaleString,
-			error: error,
-			message: error.message,
-			address: error.address,
-			code: error.code,
-			dest: error.dest,
-			errno: error.errno,
-			info: error.info,
-			path: error.path,
-			port: error.port,
-			syscall: error.syscall,
-		});
+		// Send error to myself
+		const msg = {
+			to: `anthonyvidovic@gmail.com`, // list of receivers
+			subject: `Error on TicketScout`, // Subject line
+			text: `
+			Hi Anthony, this email was automatically sent from ticketscout. There has been an error that needs to be looked at:
+
+            ${new Date().toLocaleString}
+			${error}
+			${error.message}
+			${error.address}
+			${error.code}
+			${error.dest}
+			${error.errno}
+			${error.info}
+			${error.path}
+			${error.port}
+			${error.syscall}
+			`,
+			html: `
+			Hi Anthony, this email was automatically sent from ticketscout. There has been an error that needs to be looked at:
+
+            ${new Date().toLocaleString}
+			${error}
+			${error.message}
+			${error.address}
+			${error.code}
+			${error.dest}
+			${error.errno}
+			${error.info}
+			${error.path}
+			${error.port}
+			${error.syscall}
+			`,
+		};
+
+		sendEmail(msg);
 
 		res.status(500).send(
 			"Error viewing resource, please try again at a later time. If the issue still persists please contact support"

b4db28ff361c96562f8c424d769dc9526d2c6435
Anthony Vidovic <avidovic@uoguelph.ca>
2021-10-03 17:22:31 -0400

    ssl config

--- app.js
+++ app.js
@@ -22,13 +22,12 @@ passportConfig(passport);
 app.use(express.urlencoded({ extended: false }));
 
 // Redirect to secure if request is not secure and not localhost
-if (port == process.env.PORT) {
-	app.enable("trust proxy"); // Enable reverse proxy support
-	app.use((req, res, next) => {
-		if (req.secure) next();
-		else res.redirect(301, `https://${req.headers.host}${req.url}`);
-	});
-}
+
+app.enable("trust proxy"); // Enable reverse proxy support
+app.use((req, res, next) => {
+	if (req.secure) next();
+	else res.redirect(301, `https://${req.headers.host}${req.url}`);
+});
 
 // Express Session
 app.use(
